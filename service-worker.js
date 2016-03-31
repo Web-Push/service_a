581107fa5587a096ea3732a9d6bbd4096d7ce4b4
Web-Push <cloudlearning777@gmail.com>
2016-03-31 13:26:48 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,6 @@ self.addEventListener('push', function(event) {
   });
   event.waitUntil(
     fetch('https://web-push.github.io/WebPushControl/users.json', {
-        mode: 'cors',
         credentials: 'include'
       }).then(function(response){
       if (response.status !== 200) {
