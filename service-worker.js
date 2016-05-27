523167e53f6cc8036da4d785cab21a273d5a6da8
Web-Push <cloudlearning777@gmail.com>
2016-05-27 16:21:40 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -19,10 +19,10 @@ self.addEventListener('push', function(event) {
   event.waitUntil(
     //fetch('https://web-push.github.io/WebPushControl/users.json', {
     fetch('https://pushsvsample.herokuapp.com/test'
-      //, {
-      //  mode: 'cors',
-      //  credentials: 'include'
-      //}
+      , {
+          mode: 'cors',
+          credentials: 'include'
+        }
       ).then(function(response){
       if (response.status !== 200) {
         console.log('Looks like there was a problem. Status Code: ', response.status);
