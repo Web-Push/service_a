e39ded71684c091dffc77e711e693ab025130390
Web-Push <cloudlearning777@gmail.com>
2016-05-27 19:08:57 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -17,8 +17,8 @@ self.addEventListener('push', function(event) {
   });
 
   event.waitUntil(
-    //fetch('https://web-push.github.io/WebPushControl/users.json', {
-    fetch('https://pushsvsample.herokuapp.com/test'
+    fetch('https://web-push.github.io/WebPushControl/users.json', {
+    //fetch('https://pushsvsample.herokuapp.com/test'
       , {
           mode: 'cors',
           credentials: 'include'
@@ -29,8 +29,8 @@ self.addEventListener('push', function(event) {
       } else {
         response.text().then(function(textdata) {
           console.log('text:', textdata);
-          //var jsondata = JSON.parse(textdata);
-          //checkLogin(jsondata);
+          var jsondata = JSON.parse(textdata);
+          checkLogin(jsondata);
         });
       }
     })
